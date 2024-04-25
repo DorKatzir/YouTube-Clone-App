b3733cd411089ab73f6fda9a13d827949fb2a27a
Dor Katzir <katzirdor@gmail.com>
2024-04-25 13:52:06 +0300

    created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react'
+import { Box } from '@mui/material'
 
-function App() {
-
-  return (
-    <>
-      <h1>Hello Dror</h1>
-    </>
+function App() (
+    <BrowserRouter>
+      <Box sx={{ backgroundColor: '#000' }}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={ <Feed /> } />
+          <Route path='/video/:id' element={ <VideoDetail /> } />
+          <Route path='/channel/:id' element={ <ChannelDetail /> } />
+          <Route path='/search/:searchTerm' element={ <SearchFeed /> } />
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
-}
+
 
 export default App
